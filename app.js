987c2ac1a7d97fbf39d67deea7b35d5f2f284fd4
doctafaustus <clowd150@gmail.com>
2020-07-04 22:34:16 -0500

    Code dynamic meta setter

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // Core modules
 const express = require('express');
+const cheerio = require('cheerio');
 const cors = require('cors');
 const fs = require('fs');
+
+
+// Globals
+const $ = require('cheerio');
 
 // Express app / Middleware
 const app = express();
@@ -19,13 +24,35 @@ if (!process.env.PORT) {
 
 // Keep paths using the app.html file on direct route hits
 app.use('/*', (req, res, next) => {
-  if (/^\/(api|auth)\//.test(req.originalUrl)) next();
-  else res.sendFile(`${__dirname}/client/dist/app.html`);
+  if (/^\/api\//.test(req.originalUrl)) next();
+  else console.log('afdas') || res.sendFile(`${__dirname}/client/dist/index.html`);
 });
 
-app.get('/api/test', (req, res) => {
+app.get('/api/test', async (req, res) => {
   console.log('/test');
-  res.sendFile(`${__dirname}/snippets/cleaner-settimeout-callbacks.html`);
+
+  // const snippetHTML = await fs.promises.readFile(`${__dirname}/snippets/cleaner-settimeout-callbacks.html`, 'utf-8');
+  // const $snippet = $(snippetHTML);
+  // const metaTitleTag = $snippet.find('meta[property=og\\:title]');
+  // const metaImageTag = $snippet.find('meta[property=og\\:image]');
+  // const metaDescTag = $snippet.find('meta[property=og\\:description]');
+  // const metaTags = [$.html(metaTitleTag), $.html(metaImageTag), $.html(metaDescTag)].join();
+
+
+  const snippetsText = await fs.promises.readFile(`${__dirname}/client/src/snippets.js`, 'utf-8');
+  const startPos = snippetsText.search(/\[/);
+  const endPos = snippetsText.lastIndexOf('];') + 1;
+  const trimmedSnippetText = snippetsText.substring(startPos, endPos);
+  const snippetsArr = JSON.parse(trimmedSnippetText);
+
+
+
+
+
+  const baseHTML = await fs.promises.readFile(`${__dirname}/client/dist/index.html`, 'utf-8');
+  const tempHTML = baseHTML.replace('<html lang=en>', '<article>').replace('</html>', '</article>');
+  const $base = $(tempHTML);
+  console.log('?', $base.find('meta[property=og\\:title]').length);
 });
 
 
--- client/src/snippets.js
+++ client/src/snippets.js
@@ -2,34 +2,34 @@ import utils from '@/mixins/utils';
 
 const snippets = [
   {
-    title: 'Filter Log by Script Source',
-    image: 'https://res.cloudinary.com/dormh2fvt/image/upload/v1593490980/JS%20Bits/filter-log-by-source.jpg',
-    desc: 'Remove all the garbage in the console by filtering by script source.',
+    "title": "Filter Log by Script Source",
+    "image": "https://res.cloudinary.com/dormh2fvt/image/upload/v1593490980/JS%20Bits/filter-log-by-source.jpg",
+    "desc": "Remove all the garbage in the console by filtering by script source."
   },
   {
-    title: 'Cleaner setTimeout Callbacks',
-    image: 'https://res.cloudinary.com/dormh2fvt/image/upload/v1593405909/JS%20Bits/Ebj2-ZDKWAAE08-CP.jpg',
-    desc: 'How to keep your setTimeout callbacks on one line.',
+    "title": "Cleaner setTimeout Callbacks",
+    "image": "https://res.cloudinary.com/dormh2fvt/image/upload/v1593405909/JS%20Bits/Ebj2-ZDKWAAE08-CP.jpg",
+    "desc": "How to keep your setTimeout callbacks on one line."
   },
   {
-    title: 'Argument Validation',
-    image: 'https://res.cloudinary.com/dormh2fvt/image/upload/v1593405964/JS%20Bits/Eb-Sda-XBXYAUs-Qxr-1.jpg',
-    desc: 'Here\'s a fancy trick for requiring arguments to a function.'
+    "title": "Argument Validation",
+    "image": "https://res.cloudinary.com/dormh2fvt/image/upload/v1593405964/JS%20Bits/Eb-Sda-XBXYAUs-Qxr-1.jpg",
+    "desc": "Here's a fancy trick for requiring arguments to a function."
   },
   {
-    title: 'Debugging Tip: "Continue to here"',
-    image: 'https://res.cloudinary.com/dormh2fvt/image/upload/v1593406006/JS%20Bits/continue-to-here-1.png',
-    desc: 'Inside the Chrome debugger, there\'s a simple way to advance the flow of your debugging scope.'
+    "title": "Debugging Tip: \"Continue to here\"",
+    "image": "https://res.cloudinary.com/dormh2fvt/image/upload/v1593406006/JS%20Bits/continue-to-here-1.png",
+    "desc": "Inside the Chrome debugger, there's a simple way to advance the flow of your debugging scope."
   },
   {
-    title: 'debug()',
-    image: 'https://res.cloudinary.com/dormh2fvt/image/upload/v1593406052/JS%20Bits/chrome-p6-KVK2-Mc-AH.png',
-    desc: 'Pause execution in the debugger on the first line of a provided fn.'
+    "title": "debug()",
+    "image": "https://res.cloudinary.com/dormh2fvt/image/upload/v1593406052/JS%20Bits/chrome-p6-KVK2-Mc-AH.png",
+    "desc": "Pause execution in the debugger on the first line of a provided fn."
   },
   {
-    title: 'window.onerror()',
-    image: 'https://res.cloudinary.com/dormh2fvt/image/upload/v1593406098/JS%20Bits/carbon-4.png',
-    desc: 'Easily capture and report on global errors with window.error.'
+    "title": "window.onerror()",
+    "image": "https://res.cloudinary.com/dormh2fvt/image/upload/v1593406098/JS%20Bits/carbon-4.png",
+    "desc": "Easily capture and report on global errors with window.error."
   }
 ];
 
