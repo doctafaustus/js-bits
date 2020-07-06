99762735c94cb0917452703a7ec6eab7f2c56c5d
doctafaustus <clowd150@gmail.com>
2020-07-06 00:37:14 -0500

    Add GA

--- client/src/main.js
+++ client/src/main.js
@@ -6,9 +6,13 @@ import VueHighlightJS from 'vue-highlightjs'
 
 Vue.config.productionTip = false;
 
-Vue.use(VueHighlightJS)
+Vue.use(VueHighlightJS);
+Vue.use(VueAnalytics, {
+  id: 'UA-171716275-1',
+  router
+});
 
 new Vue({
   router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
