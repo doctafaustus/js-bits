9afc724d83b6c0fd385771ac772b5cda7ddb36f8
doctafaustus <clowd150@gmail.com>
2020-09-10 10:13:57 -0500

    Add correct fonts source

--- client/src/App.vue
+++ client/src/App.vue
@@ -29,6 +29,7 @@ export default {
 
 <style lang="scss">
 @import '@/styles/_reset.scss';
+@import '@/styles/_fonts.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
