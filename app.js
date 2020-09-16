a2d3ba7872ffea5cbcd6374e5ec15a0651fb43c2
doctafaustus <clowd150@gmail.com>
2020-09-16 18:44:50 -0500

    Add compression package

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const favicon = require('serve-favicon');
-const compression = require('compression') 
+const compression = require('compression'); 
 
 
 // Globals
