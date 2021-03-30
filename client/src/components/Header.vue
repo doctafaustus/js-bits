40deeb4bfdc342b6ed2fe1fc55f79a784d56c9bf
doctafaustus <clowd150@gmail.com>
2021-03-30 17:22:12 -0500

    Add explicit width and height to header image

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <router-link to="/" class="logo-link">
-      <span class="logo-text-left">{</span><img class="js-logo" src="@/assets/js-logo.webp"><span class="logo-text-right">Bits }</span>
+      <span class="logo-text-left">{</span><img class="js-logo" src="@/assets/js-logo.webp" width="34" height="34"><span class="logo-text-right">Bits }</span>
     </router-link>
   </header>
 </template>
