2f745a30249b9a44739bedb6aceae86752b573bd
doctafaustus <clowd150@gmail.com>
2021-05-02 23:00:11 -0500

    Start bug finder # 1

--- bug-finder-routes.js
+++ bug-finder-routes.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const Cookies = require('cookies');
 
 module.exports = app => {
 
   app.get('/bug-finder/bug-1', (req, res) => {
-    console.log('/bug-1');
+    const cookie = new Cookies(req, res);
+
+    cookie.set('vipCustomerLevel', 'platinum', { expires: setDate(180) });
     res.sendFile(path.join(__dirname, 'client/public/bug-finder/bug-1.html'));
   });
 
+}
+
+
+function setDate(days = 180) {
+  let targetDate = new Date().getTime() + (days * 24 * 60 * 60 * 1000);
+  targetDate = new Date().setTime(targetDate);
+  return new Date(targetDate);
 }
