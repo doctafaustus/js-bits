157b4f2ebdc7b8dcff9c3e7d12851a31dc29c7ad
doctafaustus <clowd150@gmail.com>
2021-06-08 18:44:31 -0500

    Use base Vue code

--- client/src/main.js
+++ client/src/main.js
@@ -1,24 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
-import VueAnalytics from 'vue-analytics';
 
 
-if (navigator.userAgent.indexOf("Chrome-Lighthouse") > -1) {
-  document.querySelector('#nic-cage').style.visibility = 'visible';
-} else {
-  Vue.config.productionTip = false;
-
-  Vue.use(VueAnalytics, {
-    id: 'UA-171716275-1',
-    router
-  });
-
-  new Vue({
-    router,
-    render: h => h(App),
-  }).$mount('#app');
-
-  window.baseURL = (window.location.host.includes('jsbits-yo.com')) ? 
-  `http://${window.location.host}/api` : 'http://localhost:8081/api';
-}
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app');
