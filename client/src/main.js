e7d3e8fa5ab164db0d8e6c5f3741254f10774f3f
doctafaustus <clowd150@gmail.com>
2021-06-08 19:36:27 -0500

    Test

--- client/src/main.js
+++ client/src/main.js
@@ -7,13 +7,9 @@ const userAgent = window.navigator.userAgent;
 if (userAgent.includes('Chrome-Lighthouse')) {
   document.querySelector('#nic-cage').style.visibility = 'visible';
 } else {
-  Vue.config.productionTip = false;
-
   new Vue({
     router,
     render: h => h(App),
   }).$mount('#app');
-
-  window.baseURL = (window.location.host.includes('jsbits-yo.com')) ? 
-  `http://${window.location.host}/api` : 'http://localhost:8081/api';
 }
+
