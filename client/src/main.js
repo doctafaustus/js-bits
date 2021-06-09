6b11f4da6325a5840c6920052dd3439b596325ef
doctafaustus <clowd150@gmail.com>
2021-06-08 20:06:28 -0500

    Revert old code

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
+import VueAnalytics from 'vue-analytics';
 
-const userAgent = window.navigator.userAgent;
 
-if (userAgent.includes('Chrome-Lighthouse')) {
-  document.querySelector('#nic-cage').style.visibility = 'visible';
-} else {
-  new Vue({
-    router,
-    render: h => h(App),
-  }).$mount('#app');
-}
+Vue.config.productionTip = false;
 
+Vue.use(VueAnalytics, {
+  id: 'UA-171716275-1',
+  router
+});
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app');
+
+window.baseURL = (window.location.host.includes('jsbits-yo.com')) ? 
+`http://${window.location.host}/api` : 'http://localhost:8081/api';
